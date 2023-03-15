08a1b1227f457c57fa2e38bba7beb26041daec7f
Landon Li <lxx4work@gmail.com>
2023-03-15 23:03:14 +0800

    Custom modifications

--- src/net/tools/naive/naive_proxy_bin.cc
+++ src/net/tools/naive/naive_proxy_bin.cc
@@ -162,7 +162,7 @@ void GetCommandLine(const base::CommandLine& proc,
                     CommandLine* cmdline,
                     MultipleListenCollector& multiple_listens) {
   if (proc.HasSwitch("h") || proc.HasSwitch("help")) {
-    std::cout << "Usage: naive { OPTIONS | config.json }\n"
+    std::cout << "Usage: young { OPTIONS | config.json }\n"
                  "\n"
                  "Options:\n"
                  "-h, --help                 Show this message\n"
@@ -184,7 +184,7 @@ void GetCommandLine(const base::CommandLine& proc,
   }
 
   if (proc.HasSwitch("version")) {
-    std::cout << "naive " << version_info::GetVersionNumber() << std::endl;
+    std::cout << "young " << version_info::GetVersionNumber() << std::endl;
     exit(EXIT_SUCCESS);
   }
 
